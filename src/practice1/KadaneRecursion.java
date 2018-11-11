b794a5ffb2525ce798fee751a00740c8c7546912
poojakittur <gitpoojakittur@gmail.com>
2018-11-11 01:38:07 -0600

    Kadane's recursive algorithm

---
+++ src/practice1/KadaneRecursion.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class KadaneRecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MaxArrayApproch2 maxArrayApproch2 = new MaxArrayApproch2();
+		System.out.println(maxArrayApproch2.maxSumMethod(9, 0, 0));
+	}
+
+}
